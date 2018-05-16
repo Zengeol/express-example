7a3ffd8a58344cd2bb83a13e9b03be133297397d
Paul Bailey <paul.m.bailey@gmail.com>
2018-05-16 00:59:13 +0000

    express stuff

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-var express = require('express');
-var nunjucks = require('nunjucks');
+const express = require('express');
+const nunjucks = require('nunjucks');
+const body_parser = require('body-parser');
+const pgp = require('pg-promise')({});
+const db = pgp({database: 'blog', user: 'postgres'});
 
 var app = express();
 
@@ -9,6 +12,7 @@ nunjucks.configure('views', {
   noCache: true
 });
 
+app.use(body_parser.urlencoded({extended: false}));
 app.use(express.static('public'));
 
 app.get('/', function (request, response) {
@@ -23,16 +27,37 @@ app.get('/projects', function (request, response) {
   response.send('Projects');
 });
 
-app.get('/post/:slug', function (request, response) {
+app.get('/post/:slug', function (request, response, next) {
   var slug = request.params.slug;
-  response.send('Post About: ' + slug);
+  
+  db.one('SELECT * FROM post WHERE slug=$1', [slug])
+   .then(function (results) {
+     response.send(results);
+   })
+   .catch(next);
 });
 
 app.get('/hello', function (request, response) {
   var name = request.query.name || 'World';
-  var context = {title: 'Hello', name: name};
+  var context = {
+    title: 'Hello',
+    name: name,
+    friends: [{name: "Joan"}]
+  };
   
   response.render('index.html', context);
+});
+
+app.get('/form', function(req, resp) {
+  resp.render('form.html');
+});
+
+app.post('/submit', function (req, resp) {
+  console.log(req.body);
+  
+  //resp.render('thanks.html');
+  
+  resp.redirect('/some-where-else');
 });
 
 
